2ca57c88daeccfd15ffcb984e78fac067fedaf5f
MHeinz1996 <michael.heinzinger@gmail.com>
2022-06-06 19:57:20 -0500

    Completed methods that simulate the frames. Need to create method to calculate the score.

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player:
         self.name = name
         self.score = 0
         self.frames = []
+        self.multipliers = []
 
     def bowl(self, frame_num):
         frame = Frame(frame_num)
@@ -15,10 +16,12 @@ class Player:
 
             if(frame.first_bowl != 10):
                 frame.second_bowl = randrange(0, 11-frame.first_bowl)
-
                 self.frames.append(frame.set_frame())
+                if(frame.first_bowl+frame.second_bowl == 10):
+                    self.multipliers.append('/')
             else:
                 self.frames.append(frame.set_frame())
+                self.multipliers.append('X')
         else: # frame_num == 10
             frame.first_bowl = randrange(0, 11)
 
@@ -26,23 +29,29 @@ class Player:
                 frame.second_bowl = randrange(0, 11-frame.first_bowl)
                 
                 if(frame.first_bowl + frame.second_bowl == 10):
+                    self.multipliers.append('/')
                     frame.third_bowl = randrange(0, 11)
 
                     self.frames.append(frame.set_frame())
                 else: # frame.first_bowl + frame.second_bowl != 10
                     self.frames.append(frame.set_frame())
             else: # frame.first_bowl == 10
+                self.multipliers.append('X')
                 frame.second_bowl = randrange(0, 11)
                 
                 if frame.second_bowl != 10:
                     frame.third_bowl = randrange(0, 11-frame.second_bowl)
                     self.frames.append(frame.set_frame())
                 else:
+                    self.multipliers.append('X')
                     frame.third_bowl = randrange(0, 11)
                     self.frames.append(frame.set_frame())
     
         # Write logic to calculate the score
         # Maybe make a first in-first out queue that store multipliers and applies them to the total score
+
+        
+
 
         if frame_num == 10:
             print(self.frames)
